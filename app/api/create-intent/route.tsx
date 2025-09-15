72c13357b3c2db3a577b8b4f06ec9eb877687b52
Yash Soni <yash2165soni@gmail.com>
2025-09-15 19:36:54 +0530

    fifth commit

--- app/api/create-intent/route.tsx
+++ app/api/create-intent/route.tsx
@@ -3,7 +3,7 @@ import Stripe from "stripe"
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
   typescript:true,
-  apiVersion: "2024-06-20", // use latest stable
+  apiVersion: "2025-08-27.basil",
 })
 
 export async function POST(request: Request) {
